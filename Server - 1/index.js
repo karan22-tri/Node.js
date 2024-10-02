c346a1ff8dfcbf44735dc1e63766193425bca3b7
karan22-tri <trivedikaran657@gmail.com>
2024-10-02 14:30:43 +0530

    server-1

--- Server - 1/index.js
+++ Server - 1/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 8000;
 
 const porthandler = (req,res)=>{
-    res.write("<h1>Welcome To The Server</h1>");
+    res.write("<h1 style='color: #333; font-size: 24px; text-align: center;'>Welcome To The Server</h1>");
     res.end();
 }
 let server = http.createServer(porthandler);
