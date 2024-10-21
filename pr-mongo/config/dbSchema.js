6ac63fa90566fb6c1aa65c99ad95cb123463686a
karan22-tri <trivedikaran657@gmail.com>
2024-10-21 15:00:41 +0530

    second

--- pr-mongo/config/dbSchema.js
+++ pr-mongo/config/dbSchema.js
@@ -9,6 +9,10 @@ const schema = mongoose.Schema({
         type:String,
         required:true
     },
+    image:{
+        type:String,
+        required:true
+    },
     date:{
         type:String,
         required:true
--- pr-mongo/index.js
+++ pr-mongo/index.js
@@ -3,12 +3,25 @@ const port = 1008
 
 const app = express()
 const db = require("./config/db")
+const path = require("path")
+const fs = require("fs")
 const adminSchema = require("./config/dbSchema")
+const multer = require("multer")
 
+const Storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,"uploads/")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.fieldname + "-" + Date.now())
+    }
+})
+
+const upload = multer({ storage: Storage}).single("image")
 
 app.set("view engine","ejs")
 app.use(express.urlencoded());
-
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
 
 
 app.get("/",async(req,res)=>{
@@ -16,12 +29,16 @@ app.get("/",async(req,res)=>{
     res.render("index",{data});
 })
 
-app.post("/insert",async(req,res)=>{
+app.post("/insert",upload,async(req,res)=>{
+    req.body.image = req.file.path
     let data = await adminSchema.create(req.body)
    data && res.redirect("back")
 })
 
 app.get("/delete",async(req,res)=>{
+    let singledata = await adminSchema.findById(req.query.id)
+    console.log(singledata)
+    fs.unlinkSync(singledata.image)
     let deletedata = await adminSchema.findByIdAndDelete(req.query.id)
     deletedata && res.redirect("back")
 })
@@ -31,7 +48,12 @@ app.get("/edit",async(req,res)=>{
     singledata && res.render("edit",{singledata})
 })
 
-app.post("/update",async(req,res)=>{
+app.post("/update",upload,async(req,res)=>{
+    let img = "";
+    let singledata = await adminSchema.findById(req.body.id)
+    req.file ? img = req.file.path : img = singledata.image
+    req.file && fs.unlinkSync(singledata.image)
+    req.body.image = img
     let data = await adminSchema.findByIdAndUpdate(req.body.id,req.body)
     data && res.redirect("/")
 })
